89c731c8a222f255fa4ad5da3f135b8c6f9850cc
dezoliveira <andresoliveira@protonmail.com>
2023-04-26 16:40:50 -0300

    refactor html and js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
-document.getElementById('output').style.visibility = 'hidden'
+let output = document.getElementById('output')
+output.style.visibility = 'hidden'
 
-document.getElementById('lbsInput').addEventListener('input', function(e){
+let lbsInput = document.getElementById('lbsInput')
+let gramasOutput = document.getElementById('gramasOutput')
+let kilosOutput = document.getElementById('kilosOutput')
+let toneladasOutput = document.getElementById('toneladasOutput')
+
+function weightConverter(e) {
   let peso = e.target.value
 
-  if(document.getElementById('lbsInput').value.length === 0){
-
-    document.getElementById('output').style.visibility = 'hidden';
+  if(lbsInput.value.length === 0){
+    output.style.visibility = 'hidden';
 
   }else{
-    document.getElementById('output').style.visibility = 'visible'
+    output.style.visibility = 'visible'
   }
 
-  document.getElementById('gramasOutput').textContent = (peso/0.0022046).toFixed(2)
+  weightCalculator(peso)
+}
 
-  document.getElementById('kilosOutput').textContent = (peso/2.2046).toFixed(2)
+function weightCalculator(peso) {
+  gramasOutput.textContent = (peso/0.0022046).toFixed(2)
+  kilosOutput.textContent = (peso/2.2046).toFixed(2)
+  toneladasOutput.textContent = (peso*16).toFixed(2)
+}
 
-  document.getElementById('toneladasOutput').textContent = (peso*16).toFixed(2)
-})
-
+lbsInput.addEventListener('input', weightConverter)
 
